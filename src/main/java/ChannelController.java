cb24dc54fa9c60b507d9f8a7911bdf15add43dc0
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-03-12 11:05:52 +0100

    started building my ChannelController + comments to keep track for myself

---
+++ src/main/java/ChannelController.java
@@ -0,0 +1,23 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//setting the class as a REST-controller to deal with HTTP requests
+@RestController
+//defines base URL for every class in this controller - setting endpoint to /Channels
+@RequestMapping("/Channels")
+public class ChannelController {
+
+    //dependency injection (DI) to automatically handle the creation of an instance to this class
+    @Autowired
+    private ChannelRepository channelRepository;
+
+    //mapping GET-requests to this method, returning the list of all channels from database
+    @GetMapping
+    public List<Channel> getAllChannels() {
+        return channelRepository.findAll();
+    }
+}
