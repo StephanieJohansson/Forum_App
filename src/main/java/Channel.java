c7e5c66f34ba96236cc3acb329da5d68f205bda8
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-03-12 10:13:09 +0100

    setting the needed annotations + comments on them for my own sake. Started on the ChannelRepository class

--- src/main/java/Channel.java
+++ src/main/java/Channel.java
@@ -1,9 +1,23 @@
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+//marking the class as a JPA entity which will store it into the database
+@Entity
 public class Channel {
+
+    //marking this as primarykey
+    @Id
+    //strategy to automatically generate the value for primary key in database when a new post is created
+    //(automatically sets an uniq ID for every new channel/message)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    //annotation to define a one-to-many relationship, to contain several messages.
+    //mapping the message-list to the Channel
+    @ElementCollection
     private List<String> messages = new ArrayList<>();
 
     //constructor, getters och setters
